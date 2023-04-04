54b91529091ae581e5b577538319681d013f4314
mihaibarbulescu86 <129853897+mihaibarbulescu86@users.noreply.github.com>
2023-04-04 14:36:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit 
 streamlit.title('Cina cea de taina')
-streamlit.header('Meniu')
-streamlit.text('Pizza')
-streamlit.text('Shaorma cu de toate')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
