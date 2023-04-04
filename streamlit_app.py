33664976e26b2e31ac4d4dc2961647271e3739cc
mihaibarbulescu86 <129853897+mihaibarbulescu86@users.noreply.github.com>
2023-04-04 14:35:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title('Cina cea de taina')
 streamlit.header('Meniu')
 streamlit.text('Pizza')
-straemlit.text('Shaorma cu de toate')
+streamlit.text('Shaorma cu de toate')
